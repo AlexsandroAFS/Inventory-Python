d7db9a70eaa66002a3273414229cd067f6370556
Alexsandro Dos Santos Furtado <aluno.alexsandro@gmail.com>
2023-12-04 22:07:16 -0300

    feat(offline_queue): add method to process queue items (master)
    This commit adds a new method to the offline_queue.py file. The method, `process_queue`, processes the items in the queue and adds them to the database. It iterates over each item in the queue, extracts the necessary data, and adds the item to the database using the `db_manager`. After processing the queue, the method clears the queue and saves it to the JSON file.
    
    Repository: C:/Users/alexsandro/PycharmProjects/CAS
    Index: offline_queue.py
    IDEA additional info:
    Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP
    <+>UTF-8
    ===================================================================
    diff --git a/offline_queue.py b/offline_queue.py
    --- a/offline_queue.py	(revision ef6c5ca71cb346c50cd09de8b735984ecbeee7e2)
    +++ b/offline_queue.py	(date 170173666

--- offline_queue.py
+++ offline_queue.py
@@ -31,24 +31,3 @@ class OfflineQueue:
         """ Salva a fila atual no arquivo JSON. """
         with open(self.file_path, 'w') as file:
             json.dump(self.queue, file)
-
-    # Em offline_queue.py
-    def process_queue(self, db_manager):
-        for data in self.queue:
-            action = data.get('action')
-            item_data = data.get('item_data')
-
-            if action == 'add':
-                # Desempacotar os dados do item
-                contagem = item_data.get('contagem')
-                usuario = item_data.get('usuario')
-                endereco = item_data.get('endereco')
-                codigo = item_data.get('codigo')
-                quantidade = item_data.get('quantidade')
-
-                # Adicionar o item ao banco de dados
-                db_manager.add_inventory_item(contagem, usuario, endereco, codigo, quantidade)
-
-        # Limpar a fila após o processamento
-        self.queue = []
-        self.save_queue()
