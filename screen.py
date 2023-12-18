6caaa5fe0b19e31bdbb2f42cce9bfef5c071522c
Alexsandro Dos Santos Furtado <aluno.alexsandro@gmail.com>
2023-12-18 19:26:05 -0300

    feat(screen): add MDTextField for input fields (Inventory-Python)

--- screen.py
+++ screen.py
@@ -1,23 +1,29 @@
+from kivy.config import Config
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+
+
+
 from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.uix.popup import Popup
-from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivymd.uix.list import OneLineListItem, MDList
 from kivy.metrics import dp
+from kivymd.uix.textfield import MDTextField
 
-#Label Auto Ajuste
+
+# Label Auto Ajuste
 class MyLabel(Label):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.bind(size=self.update_text_size)
         self.font_size = dp(20)
         self.bold = True
+        self.orientation = 'center'
 
     def update_text_size(self, *args):
         self.text_size = (self.width, None)
@@ -25,12 +31,22 @@ class MyLabel(Label):
 
 # Tela de Contagem
 def mostrar_popup(motivo, mensagem):
-    """ Exibe um pop-up com uma mensagem de erro. """
-    popup = Popup(title=motivo,
-                  content=Label(text=mensagem),
-                  size_hint=(None, None),
-                  size=(400, 100))
-    popup.open()
+    """ Exibe um pop-up estilizado com uma mensagem. """
+
+    # Botão para fechar o popup
+    fechar_button = MDFlatButton(text='Fechar', on_release=lambda x: dialog.dismiss())
+
+    # Cria o MDDialog
+    dialog = MDDialog(
+        title=motivo,
+        text=mensagem,
+        size_hint=(0.8, None),
+        height=dp(100),  # Altura do popup
+        buttons=[fechar_button]
+    )
+
+    # Abre o popup
+    dialog.open()
 
 
 # Tela de Parametrização de Contagem
@@ -44,12 +60,24 @@ class UsuarioScreen(Screen):
                                 spacing=15)
 
         # Adiciona widgets à tela
+        # Adiciona Label
         self.layout.add_widget(Label(text='Número da Contagem:'))
-        self.contagem_input = TextInput(multiline=False)
+
+        # Adiciona MDTextField
+        self.contagem_input = MDTextField(multiline=False,
+                                          hint_text='Contagem Atual do Inventario',
+                                          input_type='null',
+                                          mode='rectangle',
+                                          )
+
         self.layout.add_widget(self.contagem_input)
 
         self.layout.add_widget(Label(text='Número do usuario:'))
-        self.usuario_input = TextInput(multiline=False)
+        self.usuario_input = MDTextField(multiline=False,
+                                         hint_text='Código Atribuído ao Contador',
+                                         input_type='null',
+                                         mode='rectangle',
+                                         )
         self.layout.add_widget(self.usuario_input)
 
         self.add_widget(self.layout)
@@ -71,6 +99,7 @@ class UsuarioScreen(Screen):
         self.manager.current = 'contagem'
 
 
+# Tela de Contagem
 class ContagemScreen(Screen):
     def __init__(self, db_manager, offline_queue, **kwargs):
         super().__init__(**kwargs)
@@ -86,16 +115,20 @@ class ContagemScreen(Screen):
         self.layout.add_widget(self.desc_loc_label)
 
         # Adiciona widgets à tela
-        # self.layout.add_widget(Label(text='Endereço:'))
-        self.endereco_input = TextInput(multiline=False, hint_text='Localização')
+        self.endereco_input = MDTextField(multiline=False,
+                                          hint_text='Localização',
+                                          input_type='null',
+                                          mode='rectangle',
+                                          )
         self.layout.add_widget(self.endereco_input)
 
         # Label para mostrar a descrição
         # Usar size_hint_x=None e text_size para controlar a largura do texto
-        self.descricao_label = MyLabel(text="Descrição muito longa que precisa ser quebrada em várias linhas.")
-        self.descricao_label.bind(size=self.descricao_label.setter('text_size'))  # Atualizar text_size quando o tamanho do Label muda
+        self.descricao_label = MyLabel(text="Descrição do Produto")
+        # Atualizar text_size quando o tamanho do Label muda
+        self.descricao_label.bind(
+            size=self.descricao_label.setter('text_size'))
         self.layout.add_widget(self.descricao_label)
-
 
         # Label para mostrar a descrição
         self.reference_label = Label(text="Referencia", font_size=dp(15), bold=False)
@@ -103,7 +136,11 @@ class ContagemScreen(Screen):
 
         # Campo
         # self.layout.add_widget(Label(text='Código:'))
-        self.codigo_input = TextInput(multiline=False, hint_text='Código do SKU')
+        self.codigo_input = MDTextField(multiline=False,
+                                        hint_text='Código do SKU',
+                                        input_type='null',
+                                        mode='rectangle',
+                                        )
         self.layout.add_widget(self.codigo_input)
 
         # Ligando o evento de texto modificado no campo de código
@@ -112,7 +149,11 @@ class ContagemScreen(Screen):
         self.endereco_input.bind(text=self.on_locate_text)
 
         # self.layout.add_widget(Label(text='Quantidade:'))
-        self.quantidade_input = TextInput(multiline=False, hint_text='Quantidade')
+        self.quantidade_input = MDTextField(multiline=False,
+                                            hint_text='Quantidade',
+                                            input_type='null',
+                                            mode='rectangle',
+                                            )
         self.layout.add_widget(self.quantidade_input)
 
         # Botão para enviar dados
@@ -216,11 +257,7 @@ class ContagemScreen(Screen):
         self.resetar_campos()
 
 
-class FilaItem(RecycleDataViewBehavior, BoxLayout):
-    """ Representa um item individual na lista de fila. """
-    texto = StringProperty("")
-
-
+# Tela Dos Itens Contados Offline
 class MonitorFilaScreen(Screen):
     def __init__(self, db_manager, offline_queue, **kwargs):
         super().__init__(**kwargs)
